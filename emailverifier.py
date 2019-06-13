512d16af5ec89f81b2da6ddbd8d65bebcac4819f
Pratik Anurag <38415712+pratikanurag@users.noreply.github.com>
2019-06-13 19:00:20 +0530

    Create emailverifier.py

---
+++ emailverifier.py
@@ -0,0 +1,33 @@
+import re
+import smtplib
+import dns.resolver
+
+fromAddress = 'panurag247365@gmail.com'
+regex = '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$'
+
+def verify(addressToVerify):
+    match = re.match(regex, addressToVerify)
+    if match == None:
+	    print('Bad Syntax')
+	    return
+    else:
+        splitAddress = addressToVerify.split('@')
+        domain = str(splitAddress[1])
+        print('Domain:', domain)
+        try:
+            records = dns.resolver.query(domain, 'MX')
+            mxRecord = records[0].exchange
+            mxRecord = str(mxRecord)
+            server = smtplib.SMTP()
+            server.set_debuglevel(0)
+            server.connect(mxRecord)
+            server.helo(server.local_hostname) 
+            server.mail(fromAddress)
+            code, message = server.rcpt(str(addressToVerify))
+            server.quit()
+        except:
+            return
+    if code == 250:
+	    return True
+    else:
+	    return False
